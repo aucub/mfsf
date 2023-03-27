db6a4ed839d249e9cb80db7399fa71a523ede147
aucub <78630225+aucub@users.noreply.github.com>
2023-03-27 22:18:32 +0800

    update

--- src/app/routes/user/page-list/page-list.component.ts
+++ src/app/routes/user/page-list/page-list.component.ts
@@ -1,10 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
-import { STColumn, STComponent, STData } from '@delon/abc/st';
+import { STChange, STColumn, STComponent, STData } from '@delon/abc/st';
 import { SFSchema } from '@delon/form';
 import { ModalHelper, _HttpClient } from '@delon/theme';
-import { PageListRes } from '@sta';
-//import { constructor } from 'jasmine';
-//import {add} from "husky";
 
 @Component({
   selector: 'app-user-page-list',
@@ -17,11 +14,14 @@ export class UserPageListComponent {
   allInBody = true;
   params = { keyword: '' };
   resReName = { list: 'data.records' };
-  //process = (data: STData[], rawData?: PageListRes) => rawData?.data.records as STData[];
+
+  change(e: STChange): void {
+    console.log('change', e);
+  }
 
   searchSchema: SFSchema = {
     properties: {
-      username: {
+      keyword: {
         type: 'string',
         title: '用户名'
       }
@@ -29,18 +29,45 @@ export class UserPageListComponent {
   };
   @ViewChild('st') private readonly st!: STComponent;
   columns: STColumn[] = [
+    { title: '编号', type: 'checkbox' },
     { title: '用户名', index: 'username' },
-    { title: '昵称', index: 'nickname' },
+    { title: '昵称', index: 'nickname', render: 'nicknameTpl' },
     { title: '上次登录时间', index: 'loginTime' },
-    { title: '备注', index: 'note' },
+    { title: '备注', index: 'note', render: 'noteTpl' },
     {
       title: '',
       buttons: [
-        { text: '查看', click: (item: any) => `/form/${item.id}` }
+        {
+          text: `Edit`,
+          iif: i => !i.edit,
+          click: i => this.updateEdit(i, true)
+        },
+        {
+          text: `Save`,
+          iif: i => i.edit,
+          click: i => {
+            this.submit(i);
+          }
+        },
+        {
+          text: `Cancel`,
+          iif: i => i.edit,
+          click: i => this.updateEdit(i, false)
+        }
+        //{ text: '查看', click: (item: any) => `/form/${item.id}` }
         // { text: '编辑', type: 'static', component: FormEditComponent, click: 'reload' },
       ]
     }
   ];
+
+  private submit(i: STData): void {
+    JSON.stringify(this.st.pureItem(i));
+    this.updateEdit(i, false);
+  }
+
+  private updateEdit(i: STData, edit: boolean): void {
+    this.st.setRow(i, { edit }, { refreshSchema: true });
+  }
 
   constructor(private http: _HttpClient, private modal: ModalHelper) {}
 
