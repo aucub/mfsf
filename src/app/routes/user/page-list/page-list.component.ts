4138a3e32f86495a2dff172cf5ed04db3e478e16
aucub <78630225+aucub@users.noreply.github.com>
2023-03-27 21:28:54 +0800

    update pageList

--- src/app/routes/user/page-list/page-list.component.ts
+++ src/app/routes/user/page-list/page-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { STColumn, STComponent } from '@delon/abc/st';
+import { Component, ViewChild } from '@angular/core';
+import { STColumn, STComponent, STData } from '@delon/abc/st';
 import { SFSchema } from '@delon/form';
 import { ModalHelper, _HttpClient } from '@delon/theme';
 import { PageListRes } from '@sta';
+//import { constructor } from 'jasmine';
+//import {add} from "husky";
 
 @Component({
   selector: 'app-user-page-list',
-  templateUrl: './page-list.component.html',
+  templateUrl: './page-list.component.html'
 })
-export class UserPageListComponent implements OnInit {
+export class UserPageListComponent {
   url = `/user/pageList`;
+  method = 'POST';
+  reName = { pi: 'pageNum', ps: 'pageSize' };
+  allInBody = true;
+  params = { keyword: '' };
+  resReName = { list: 'data.records' };
+  //process = (data: STData[], rawData?: PageListRes) => rawData?.data.records as STData[];
+
   searchSchema: SFSchema = {
     properties: {
       username: {
@@ -22,39 +31,22 @@ export class UserPageListComponent implements OnInit {
   columns: STColumn[] = [
     { title: '用户名', index: 'username' },
     { title: '昵称', index: 'nickname' },
-    { title: '上次登录时间',  index: 'loginTime' },
+    { title: '上次登录时间', index: 'loginTime' },
     { title: '备注', index: 'note' },
     {
       title: '',
       buttons: [
-         { text: '查看', click: (item: any) => `/form/${item.id}` },
+        { text: '查看', click: (item: any) => `/form/${item.id}` }
         // { text: '编辑', type: 'static', component: FormEditComponent, click: 'reload' },
       ]
     }
   ];
 
-  constructor(private http: _HttpClient, private modal: ModalHelper) { }
-
-  ngOnInit(): void { 
-    /* this.http.post('/user/pageList', {
-      pageNum: 1,
-      pageSize: 10,
-      keyword: ''
-    }).subscribe((res: PageListRes) => {
-    
-      //打印后端返回的结果
-      console.log(res)
-      
-      //将返回的结果list赋给data
-      this.st.data = res.data.records;
-    }); */
-  }
+  constructor(private http: _HttpClient, private modal: ModalHelper) {}
 
   add(): void {
     // this.modal
     //   .createStatic(FormEditComponent, { i: { id: 0 } })
     //   .subscribe(() => this.st.reload());
-
   }
-
 }
